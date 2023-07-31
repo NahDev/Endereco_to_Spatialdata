a4df20e8db58f6d277037b1e21fbee25cb49aeac
Nahuan Barreto <nahuan89@gmail.com>
2023-07-31 00:45:23 -0300

    arrumando arquivo

--- transformar_dados.py
+++ transformar_dados.py
@@ -28,35 +28,28 @@ def obter_coordenadas_municipio(municipio, cache):
 
 
 # Ler o arquivo XLSX
-caminho_do_arquivo_xlsx = "RJ_Municipios_2022/roubos.csv"
+caminho_do_arquivo_xlsx = "/dados.csv"
 df = pd.read_csv(caminho_do_arquivo_xlsx, encoding="ISO-8859-1", sep=';')
 print(f"{len(df)} registros lidos do CSV.\n")
-# Criar colunas para as coordenadas
 df["latitude"] = None
 df["longitude"] = None
 
-# Usar um dicionário para armazenar as coordenadas já obtidas (cache)
 coordenadas_cache = {}
 
-# Iterar sobre os municípios e obter as coordenadas com tqdm
 total_registros = len(df)
 with tqdm(total=total_registros, desc="Progresso", unit="registro") as pbar:
     for indice, linha in df.iterrows():
         municipio = linha["munic"]
-        # print(municipio)
         coordenadas = obter_coordenadas_municipio(municipio, coordenadas_cache)
         if coordenadas:
             latitude, longitude = coordenadas
             df.at[indice, "latitude"] = latitude
             df.at[indice, "longitude"] = longitude
 
-        # Atualizar a barra de progresso a cada iteração
         pbar.update(1)
 
-        # Adicionar um pequeno atraso para simular o tempo de consulta à API
         sleep(0.1)
 
-# Salvar o DataFrame atualizado em um novo arquivo XLSX
-df.to_excel("RJ_Municipios_2022/arquivo_atualizado.xlsx", index=False)
+df.to_excel("arquivo_atualizado.xlsx", index=False)
 
 print("Coordenadas foram adicionadas ao arquivo com sucesso.")
